8dc9a9c6dfd49f0da044e75683aa2e514dba04a3
Ruslan Kyba <kybargr@gmail.com>
2017-03-14 13:51:38 +0200

    24

--- src/components/App.js
+++ src/components/App.js
@@ -28,13 +28,13 @@ class App extends Component {
           meta={[
             { name: 'description', content: 'React starter kit based on Atomic Design with React Router v4, Webpack, Redux, Server Side Rendering and more.' },
             { property: 'og:site_name', content: 'ARc' },
-            { property: 'og:image', content: 'https://diegohaz.github.io/arc/thumbnail.png' },
+            { property: 'og:image', content: 'thumbnail.png' },
             { property: 'og:image:type', content: 'image/png' },
             { property: 'og:image:width', content: '1200' },
             { property: 'og:image:height', content: '630' },
           ]}
           link={[
-            { rel: 'icon', href: 'https://diegohaz.github.io/arc/icon.png' },
+            { rel: 'icon', href: 'icon.png' },
           ]}
         />
         {children}
